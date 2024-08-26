d5bc67d5ed983786d922ed08e131699648035e61
bboyc <bboyc@DESKTOP-N0RDKUL>
2024-08-25 22:02:09 -0500

    Netbeans ejemplo

---
+++ src/ensayo_5/Ensayo_5.java
@@ -0,0 +1,18 @@
+
+ package ensayo_5;
+ import java.util.Scanner;  
+ import java.lang.Math;  
+ public class Ensayo_5 {
+  public static void main(String[] args) { 
+  Scanner lectura= new Scanner(System.in);  
+  System.out.print("Ingrese la base del triángulo (cm): ");  
+  double base = lectura.nextDouble();  
+  System.out.print("Ingrese la altura del triángulo (cm): ");  
+  double altura = lectura.nextDouble();   
+  double area = (base * altura) / 2;    
+  System.out.println("Área de un triángulo");  
+  System.out.println("El área del siguiente triángulo con base " + base + " cm y altura de " + altura + " cm es:");  
+  System.out.println("Área = (b × h) / 2 = (" + base + " × " + altura + ") / 2 = " + area + " cm²");  
+      
+    }
+   }
